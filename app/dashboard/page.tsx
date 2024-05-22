07e29852c7657c3655d49038ab7a6df47af623da
Duarte Fernandes <duartejose99@hotmail.com>
2024-05-22 01:37:18 +0100

    project_setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div className="mx-auto grid w-full max-w-6xl gap-2">
+      <h1 className="text-3xl font-semibold">Dashboard</h1>
+    </div>
+  );
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function RegisterPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <p className="text-balance text-muted-foreground">Enter your information to create an account</p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="Lewis" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Hamilton" required />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="email@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Confirm Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/login" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/logo.svg"
+          alt="CarIn"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-contain p-12 dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
