ecf15ad20b1163f6ddb65fb663cecab614492ccf
Duarte Fernandes <duartejose99@hotmail.com>
2024-06-16 17:43:40 +0100

    Implement `Register`

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,41 +1,116 @@
 "use client";
 
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import AuthForm from "@/components/ui/authForm";
+import { useAuth } from "@/hooks/useAuth";
+import { useToast } from "@/components/ui/use-toast";
+import { Calendar } from "@/components/ui/calendar";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
 export default function RegisterPage() {
+  const { register } = useAuth();
+  const router = useRouter();
+  const { toast } = useToast();
+  const [birthDate, setBirthDate] = useState<Date | undefined>(undefined);
+
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const firstName = firstNameRef.current?.value;
+    const lastName = lastNameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const confirmPassword = confirmPasswordRef.current?.value;
+
+    if (!firstName || !lastName || !email || !password || !confirmPassword || !birthDate) {
+      toast({
+        title: "Error",
+        description: "All fields are required!",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      toast({
+        title: "Error",
+        description: "Passwords do not match!",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    try {
+      await register({ firstName, lastName, birthDate: birthDate.toISOString(), email, password });
+      toast({
+        title: "Registration Success",
+        description: "Your account has been created!",
+      });
+      router.push("/auth/login");
+    } catch (err: any) {
+      toast({
+        title: "Registration Error",
+        description: err.message,
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <AuthForm>
       <div className="grid gap-2 text-center">
         <h1 className="text-3xl font-bold">Sign Up</h1>
         <p className="text-balance text-muted-foreground">Enter your information to create an account</p>
       </div>
-      <form className="grid gap-4">
+      <form className="grid gap-4" onSubmit={handleSubmit}>
         <div className="grid grid-cols-2 gap-4">
           <div className="grid gap-2">
             <Label htmlFor="first-name">First name</Label>
-            <Input id="first-name" placeholder="Lewis" type="text" required />
+            <Input id="first-name" placeholder="Lewis" type="text" ref={firstNameRef} required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="last-name">Last name</Label>
-            <Input id="last-name" placeholder="Hamilton" type="text" required />
+            <Input id="last-name" placeholder="Hamilton" type="text" ref={lastNameRef} required />
           </div>
         </div>
         <div className="grid gap-2">
+          <Label htmlFor="birthdate">Birthdate</Label>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant={"outline"} className={cn("w-full justify-start text-left font-normal", !birthDate && "text-muted-foreground")}>
+                {birthDate ? format(birthDate, "PPP") : <span>Pick a date</span>}
+                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar mode="single" selected={birthDate} onSelect={setBirthDate} disabled={(date) => date > new Date()} initialFocus />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" placeholder="email@example.com" type="email" required />
+          <Input id="email" placeholder="email@example.com" type="email" ref={emailRef} required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" minLength={8} placeholder="********" type="password" required />
+          <Input id="password" minLength={8} placeholder="********" type="password" ref={passwordRef} required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="confirm-password">Confirm Password</Label>
-          <Input id="confirm-password" minLength={8} placeholder="********" type="password" required />
+          <Input id="confirm-password" minLength={8} placeholder="********" type="password" ref={confirmPasswordRef} required />
         </div>
         <Button type="submit" className="w-full">
           Create an account
